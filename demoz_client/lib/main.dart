c45798f618f30b4de35d8460e491c81207d92a92
RediateB <it.rediate.befekadu@gmail.com>
2021-09-08 12:07:58 +0300

    main file updated

--- demoz_client/lib/main.dart
+++ demoz_client/lib/main.dart
@@ -2,25 +2,33 @@ import 'package:demoz_client/auth/login/bloc/login_bloc.dart';
 import 'package:demoz_client/auth/login/screens/loginScreen.dart';
 import 'package:demoz_client/auth/register/bloc/register_bloc.dart';
 import 'package:demoz_client/auth/register/screens/registerScreen.dart';
-import 'package:demoz_client/expense/screens/expense_detail_screen.dart';
+import 'package:demoz_client/expense/bloc/expense_bloc.dart';
+import 'package:demoz_client/expense/data_provider/expense_data.dart';
+import 'package:demoz_client/expense/repository/expense_summery_repository.dart';
+import 'package:demoz_client/expense/screens/expense_summery_screen.dart';
 import 'package:demoz_client/saving/bloc/saving_bloc.dart';
+import 'package:demoz_client/saving/bloc/saving_detail_bloc.dart';
+import 'package:demoz_client/saving/bloc/saving_detail_state.dart';
 import 'package:demoz_client/saving/bloc/saving_state.dart';
-import 'package:demoz_client/saving/data_provider/saving_data.dart';
+import 'package:demoz_client/saving/data_provider/saving_data_provider.dart';
 import 'package:demoz_client/saving/repository/saving_repository.dart';
+import 'package:demoz_client/saving/screens/saving_details_screen.dart';
+import 'package:demoz_client/saving/screens/saving_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
-import 'expense/screens/expense_summery_screen.dart';
-import 'saving/screens/saving_details_screen.dart';
-import 'saving/screens/saving_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final SavingRepository savingRepository =
-      SavingRepository(savingDataProvider: SavingDataProvider(http.Client()));
+  final ExpenseRepository expenseRepository = ExpenseRepository(
+      expenseDataProvider: ExpenseDataProvider(http.Client()));
+
+  final SavingRepository savingRepository = SavingRepository(
+      savingDataProvider: SavingDataProvider(httpClient: http.Client()));
+
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
@@ -28,7 +36,19 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (ctx) => LoginBloc()),
         BlocProvider(create: (ctx) => RegisterBloc()),
         BlocProvider(
-            create: (ctx) => SavingBloc(SavingUnloaded(), savingRepository))
+          create: (cxt) => ExpenseBloc(expenseRepository: expenseRepository),
+        ),
+        BlocProvider(
+            create: (cxt) =>
+                ExpenseCategoryBloc(expenseRepository: expenseRepository)),
+        BlocProvider(
+            create: (cxt) => SavingBloc(SavingUnLoaded(), savingRepository)),
+        BlocProvider(
+            create: (cxt) =>
+                SavingCreationBloc(SavingFormEmpty(), savingRepository)),
+        BlocProvider(
+            create: (cxt) =>
+                SavingDetailBloc(SavingDetailUnloaded(), savingRepository))
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -37,11 +57,9 @@ class MyApp extends StatelessWidget {
         routes: {
           '/login': (context) => LoginScreen(),
           '/register': (context) => RegisterScreen(),
-          '/home': (context) => ExpenseSummeryScreen(),
-          '/expense_detial': (context) => ExpenesDetailScreen(),
+          '/expense': (context) => ExpenseSummeryScreen(),
           '/saving': (context) => SavingScreen(),
           '/saving_detail': (context) => SavingDetailScreen(),
-          // '/profilePage': (context) => ProfileScreen(),
         },
       ),
     );
